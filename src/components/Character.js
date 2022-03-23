d8ebf7af81341db12f322c8fe6a419a718a0e8ed
Weslei Lucas <wesleibl@gmail.com>
2022-03-23 18:49:24 -0300

    Conexão com API agora esta completa, retornando tanto os dados do personagem mas até 3 episodios em que ele participou. Também foi feito o ajuste no css do Character para um Layout melhor para o visual.

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,7 +5,10 @@ import styles from '../components/css/Character.module.css'
 function Character({id}){
 
     const [char, setChar] = useState([])
-    const [planetOrigin, setPlanetOrigin] = useState([])
+    const [charOrigin, setCharOrigin] = useState([])
+    const [charEpisodes, setcharEpisodes] = useState([])
+    
+    const episodes = [];
 
     //character
     useEffect(() => {
@@ -17,18 +20,56 @@ function Character({id}){
         })
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data)
             setChar(data)
+            setCharOrigin(data.origin.name)
+            
+            //Episodes
+            data.episode.length = 3
+            for (let i = 0; i < data.episode.length; i++){
+                episodes.push(data.episode[i])
+                episodes.slice(0,2);
+
+                fetch(episodes[i] , {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then((resp) => resp.json())
+                    .then(data => {
+                        setcharEpisodes((prev) => [...prev, data])
+                    })
+                    .catch((err) => console.log(err))
+            }
         })
         .catch((err) => console.log(err))
     },[id])
 
     return (
         <div className={styles.character_main}>
-        <div>
-            <p>Name: <span>{char.name}</span></p>
-            <img src={char.image} alt={`${char.name}_img`}/>
-        </div>
+            <div className={styles.name}>
+                <h2>{char.name}</h2>
+                <img src={char.image} alt={`${char.name}_img`}/>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.status}>
+                    <p>Status: <span>{char.status}</span></p>
+                </div>
+                <div className={styles.origin}>
+                    <p>Planeta de origem: <span>{charOrigin}</span></p>
+                </div>
+                <div className={styles.episodes}>
+                    <h4>Episódios</h4>
+                    {charEpisodes.name}
+                    {charEpisodes.map(episode => 
+                        <div className={styles.episodes} key={episode.id}>
+                        <p><span>{episode.name}</span></p> 
+                        <p>Data de lançamento: {episode.air_date}</p>
+                        <p>Numero: {episode.episode}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
